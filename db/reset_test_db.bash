7c6d9c0c4772c0f00e0522b2344b0fa0c7ec4aee
joakim <joakim_mikaelsson@hotmail.se>
2020-09-28 16:53:38 +0100

    Fixing testing

--- db/reset_test_db.bash
+++ db/reset_test_db.bash
@@ -2,3 +2,6 @@ $(> db/test.sqlite)
 cat db/migrate_test.sql | sqlite3 db/test.sqlite
 
 cat db/inserts.sql | sqlite3 db/test.sqlite
+
+
+cat db/test.sql | sqlite3 db/test.sqlite
---
+++ db/test.sql
@@ -0,0 +1,1 @@
+SELECT * FROM users WHERE email = 'chai@test.nu';
