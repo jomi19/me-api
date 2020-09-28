80667f644e46819023a7fc057c00e668675d4fd0
joakim <joakim_mikaelsson@hotmail.se>
2020-09-28 18:06:24 +0100

    Fixing tests

--- test/auth.js
+++ test/auth.js
@@ -30,6 +30,7 @@ describe("Test user functions", () => {
                             res.should.have.status(test.status);
                             res.body.errors.title.should.equal(test.message);
                         } else {
+                            console.log(res);
                             res.body.data.user.token.should.be.a("string");
                             res.body.data.message.should.equal(test.message);
                         }
